263eadb762a2da16179693e88cea151689365bd6
Giwoo Lee <gglee89@gmail.com>
2016-02-23 21:30:30 +0900

    Added code according to the specifications. Collision handling is still missing

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(enemy) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = -101;
+    this.y = 101;
+    this.speed = 100;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + ( this.x * dt );
+    this.y = this.y + ( this.y * dt );
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 202;
+    this.y = 505;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+    this.x = this.x + ( this.x * dt );
+    this.y = this.y + ( this.y * dt );
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(allowedKeys) {
+    switch(allowedKeys) {
+        case 'left':
+            this.x -= this.x;
+            break;
+        case 'up':
+            this.y += this.y;
+            break;
+        case 'right':
+            this.x += this.x;
+            break;
+        case 'down':
+            this.y -= this.y;
+            break;
+        default:
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
 
+var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
